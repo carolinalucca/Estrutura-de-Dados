269201e16ec18eca3530841f87787f546717ffb9
IHMHR <martinelli.igor@hotmail.com>
2016-04-20 22:05:22 -0300

    Criando Menu e Aux Pointer
    Criando Menu e Ponteiro auxiliar para manipulação das transições das repetições.

--- Exercicio.c
+++ Exercicio.c
@@ -3,6 +3,7 @@
 #include <locale.h>
 
 int n;
+int menu();
 void popularOrdenarArray(int *pont);
 
 int main() {
@@ -13,26 +14,48 @@ int main() {
 	
 	int array[n];
 	
+	//int opcao = menu();
+	
 	popularOrdenarArray(array);
 	
 	return 0;
 }
 
+int menu() {
+	int op = 0;
+	do {
+		system("cls");
+		printf("*****************************************************************\n");
+		printf("1 - Preencher o vetor e mostrar ordenado\n");
+		printf("2 - Mostrar maior elemento do vetor\n");
+		printf("3 - Mostrar menor elemento do vetor\n");
+		printf("4 - Mostrar elementos do meio\n");
+		printf("5 - Informar se o valor esta presente no vetor\n");
+		printf("6 - Informar quantas vezes o valor apresenta no vetor\n");
+		printf("7 - Mostrar ordem inversa\n");
+		printf("8 - Unir 2 vetores em um terceiro vetor\n");
+		printf("9 - Unir 2 vetores em um terceiro vetor, mantendo ordenado\n");
+		printf("10 - Sair da aplicação\n");
+		printf("\n");
+		printf("escolha a opção: ");
+		scanf("%d", &op);
+	} while (op == 0 || op < 0 || op > 10);
+}         
+          
 void popularOrdenarArray(int *pont) {
+	int *aux;
+	aux = pont;
 	//input values for the array
 	for(int i = 0; i < n; i++) {
 		printf("Informe o valor para a %dº posição: ", i+1);
 		scanf("%d", pont);
 		pont++; //'walking' with the pointer 'index'
 	}
-	for(int i = 0; i < n; i++) 
-		pont--; //bring the pointer to the first place
-	printf("\n");
 	//sort an array
 	
 	printf("\n");
 	for(int i = 0; i < n; i++) {
-		printf("O valor na %dº posição é: %d\n", i+1, *pont);
-		pont++;
+		printf("O valor na %dº posição é: %d\n", i+1, *aux);
+		aux++;
 	}
 }
