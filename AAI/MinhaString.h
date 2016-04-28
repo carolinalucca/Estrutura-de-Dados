aac4e1c4e4d70347d0c401fa1ffe825f936a572b
IHMHR <martinelli.igor@hotmail.com>
2016-04-28 01:53:49 -0300

    "Classe" Minha String
    Começando a implementação da 'classe' Minha String, com funções da
    classe String

---
+++ AAI/MinhaString.h
@@ -0,0 +1,18 @@
+#include <string.h>
+#include <stdlib.h>
+
+bool equals(char[] *string);
+bool equalsIgnoreCase(char[] *string);
+char[] substring(int posInicial, int posFinal);
+char charAt(int posicao);
+int compareTo()(char[] *string;
+int compareToIgnoreCase(char[] *string);
+int length();
+void toUpperCase();
+void toLowerCase();
+void replace(char velhoCaracter, char novoCaracter);
+
+int main() {
+	
+	return 0;
+}
