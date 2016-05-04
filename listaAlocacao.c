0a8baaa1d54f698af6e3ffd49d91aa7ead857f1f
IHMHR <martinelli.igor@hotmail.com>
2016-05-03 22:57:32 -0300

    Entregue
    Atividade entregue pelo sinef

--- listaAlocacao.c
+++ listaAlocacao.c
@@ -6,17 +6,19 @@ int * alocarVetor(int m);
 void ordenarVetor(int *array);
 
 int main() {
-	setlocale(LC_ALL, "portuguese");
+	setlocale(LC_ALL, "portuguese"); //to use some words
 	
-	int m;
+	int m; //parametro do exercicio 1
 	printf("Informe o valor de 'm': ");
 	scanf("%d", &m);
 	
 	int *vetor = alocarVetor(m);
 	ordenarVetor(*vetor);
 	
+	printf("\n");
+	
 	int i;
-	for(i=0;i<m;i++) {
+	for(i=0;i<m;i++) { //impresão do mesmo na tela
 		printf("%d\n",*vetor);
 		vetor++;
 	}
@@ -25,24 +27,24 @@ int main() {
 }
 
 int * alocarVetor(int m) {
-	int array[m];
+	int array[m];//alocando um vetor de parametro m
 	
 	int i;
-	for (i=0;i<m;i++) {
+	for (i=0;i<m;i++) { //preenchendo o mesmo
 		printf("Informe o valor para a posição [%d]:", i+1);
 		scanf("%d", array);
 	}
 	
-	return array;
+	return *array; //retorna o pointer para o vetor
 }
 
-void ordenarVetor(int *array) {
+void ordenarVetor(int *array) { //ordenação do vetor ASC
 	int i, j, temp;
 	int *aux, *auxProx;
 	for (i=0;i<5;i++)
     {
-    	auxProx=array;
-    	aux=auxProx++;
+    	aux = array;
+    	auxProx = array + 1;
         for (j=0;j<4;j++)
         {
             if (*aux > *auxProx)
