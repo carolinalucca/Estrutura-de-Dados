d26bf0c101a3ba85d1a7dac29936707301c984d9
IHMHR <martinelli.igor@hotmail.com>
2016-05-04 22:29:54 -0300

    Consistencia e Impressão
    Colocando consistência e corrigindo a impressão na tela.

--- Parte1.c
+++ Parte1.c
@@ -9,21 +9,25 @@ int main() {
 	setlocale(LC_ALL, "portuguese");
 	int linha = 0, coluna = 0;
 	
-	printf("Informe a quantidade(s) de linha(s):");
-	scanf("%d", &linha);
-	printf("Informe a quantidade(s) de coluna(s):");
-	scanf("%d", &coluna);
+	do {
+		printf("Informe a quantidade(s) de linha(s):");
+		scanf("%d", &linha);
+		printf("Informe a quantidade(s) de coluna(s):");
+		scanf("%d", &coluna);
+	} while (linha < 1 || coluna < 1);
+	printf("\n");
 	
 	float **matriz = Alocar_matriz_dinamica(linha, coluna);
+	printf("\n");
 	
 	float **aux = matriz;
 	int i, j;
 	for(i = 0; i < linha; i++) {
 		for(j = 0; j < coluna; j++) {
-			printf("Informe o valor para a posição [%d][%d] = %.2f \t", i+1, j+1, **(aux+j) );
+			printf("Informe o valor para a posição [%d][%d] = %.2f \t", i+1, j+1, *(*(aux+i)+j) );
 		}
 		printf("\n");
-		**(aux+i);
+	
 	}
 	
 	//Parte 2
@@ -53,8 +57,17 @@ float **Alocar_matriz_dinamica(int l, int c) {
 
 
 float **Liberar_matriz_dinamica(int l, float **v) {
+	if(v == NULL) {
+		return (NULL);
+	} else if(l < 1) {
+		printf("QUANTIDADE DE LINHA(S) INCORRETO !");
+		return (v);
+	}
+	
 	int i;
 	float **matriz = v;
 	for (i = 0; i < l; i++)
 		free(matriz[i]);
+		
+	return v;
 }
