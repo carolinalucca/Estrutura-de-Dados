1a2d9f88c11df90dd95a3801db2f620585c06045
IHMHR <martinelli.igor@hotmail.com>
2016-05-06 02:02:26 -0300

    Revert "Escopo dos metodos Cliente"
    This reverts commit 2d6a8ca347fdbf24f4ee01d08923fef1029f2e75.

--- Parte1.c
+++ Parte1.c
@@ -7,7 +7,7 @@ float **Liberar_matriz_dinamica(int l, float **v);
 
 int main() {
 	setlocale(LC_ALL, "portuguese");
-	/*int linha = 0, coluna = 0;
+	int linha = 0, coluna = 0;
 	
 	do {
 		printf("Informe a quantidade(s) de linha(s):");
@@ -15,48 +15,24 @@ int main() {
 		printf("Informe a quantidade(s) de coluna(s):");
 		scanf("%d", &coluna);
 	} while (linha < 1 || coluna < 1);
-	printf("\n");*/
-	
-	printf("Preenchendo a MATRIZ 1\n");
-	float **matriz1 = Alocar_matriz_dinamica(4,3);
 	printf("\n");
-	printf("Preenchendo a MATRIZ 2\n");
-	float **matriz2 = Alocar_matriz_dinamica(4,3);
-	printf("\n");printf("\n");
 	
+	float **matriz = Alocar_matriz_dinamica(linha, coluna);
+	printf("\n");
 	
-	/*float **aux = matriz1;
+	float **aux = matriz;
 	int i, j;
-	for(i = 0; i < 4; i++) {
-		for(j = 0; j < 3; j++) {
-			printf("Informe o valor para a posição [%d][%d] = %.2f \t", i+1, j+1, *(*(aux+i)+j) );
+	for(i = 0; i < linha; i++) {
+		for(j = 0; j < coluna; j++) {
+			printf("Informe o valor para a posiÃ§Ã£o [%d][%d] = %.2f \t", i+1, j+1, *(*(aux+i)+j) );
 		}
 		printf("\n");
-	}
-	printf("\n");printf("\n");
-	float **aux2 = matriz2;
-	for(i = 0; i < 4; i++) {
-		for(j = 0; j < 3; j++) {
-			printf("Informe o valor para a posição [%d][%d] = %.2f \t", i+1, j+1, *(*(aux2+i)+j) );
-		}
-		printf("\n");
-	}*/
 	
-	float **aux1 = matriz1;
-	float **aux2 = matriz2;
-	int i, j;
-	for(i = 0; i < 4; i++) {
-		for(j = 0; j < 3; j++) {
-			printf("Mtz1 [%d][%d] + Mtz 2 [%d][%d] = %.2f \t", i+1, j+1, i+1, j+1, (*(*(aux1+i)+j) + *(*(aux2+i)+j)) );
-		}
-		printf("\n");
 	}
 	
-	Liberar_matriz_dinamica(4, matriz1);
-	Liberar_matriz_dinamica(4, matriz2);
+	//Parte 2
 	
-	printf("\n");printf("\n");
-	system("pause");
+	Liberar_matriz_dinamica(linha, matriz);
 	return 0;
 }
 
@@ -67,11 +43,11 @@ float **Alocar_matriz_dinamica(int l, int c) {
 	int i;
 	for (i = 0; i < l; i++)
 		matriz[i] = (float *) malloc(l * sizeof(float));
-	
+		
 	int j;
 	for(i = 0; i < l; i++) {
 		for(j = 0; j < c; j++) {
-			printf("Informe o valor para a posição [%d][%d]", i+1, j+1);
+			printf("Informe o valor para a posiÃ§Ã£o [%d][%d]", i+1, j+1);
 			scanf("%f", &(*(*(matriz+i)+j)));
 		}
 	}
@@ -95,4 +71,3 @@ float **Liberar_matriz_dinamica(int l, float **v) {
 		
 	return v;
 }
-
